0a4328953ae7286420451d29b0fe6854852c0bc2
YocelinGR <garcia_romero.y@hotmail.com>
2018-06-03 22:16:15 -0500

    Ejercicio Unidad 1 JS Edad a segundos

---
+++ app.js
@@ -0,0 +1,5 @@
+var age=parseInt(prompt("¿Cuál es tu edad en años?"));
+//Convirtiendo edad a segundos
+var ageInSeconds=age*365*24*60*60;
+//Mostrando la edad en la web
+document.write("Tu edad es: "+age+" y en segundos seria: "+ageInSeconds)
